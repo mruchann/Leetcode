81f143b056638865f1906c81f542a8546ddea231
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-15 02:25:42 +0300

    Create 876.cpp

---
+++ 876.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        
+        int size = 0;
+        ListNode *iterator = head;
+        
+        while (iterator != nullptr) // finding the size of the linked list.
+        {
+            size++;
+            iterator = iterator->next;
+        }
+        int i = 0;
+        while (i < size/2)
+        {
+            head = head->next;
+            i++;
+        }
+        return head;
+    }
+};
