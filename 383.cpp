e35ca6fddc979a373671e0e0cf53706f5b659e4b
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-15 04:14:58 +0300

    Update 383.cpp

--- 383.cpp
+++ 383.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char, int> umap;
+        
+        // Mapping from characters to the number of occurences of these characters in magazine.
+        
+        for (auto &item: magazine) umap[item]++;
+        
+        for (auto &item: ransomNote)
+            if (!umap[item]--) return false;
+        
+        return true;
+    }
+        
+        
+};
