7b1df98f0db2cd73cb8929f2a02a34ba38f2f0da
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-21 04:13:56 +0300

    Update 242.cpp

--- 242.cpp
+++ 242.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        
         unordered_map<char, int> umap;
         if (s.size() != t.size()) return false; // short circuit
+        // size = s.size() = t.size()
+        
         for (int i = 0; i < s.size(); i++)
+        {
             umap[s[i]]++; // addition
-        for (int i = 0; i < t.size(); i++)
             umap[t[i]]--; // consumption
+        }
+
         for (auto& item: umap)
             if (item.second) // values except zero evaluate to true
                 return false;
+        
         return true;
     }
     
