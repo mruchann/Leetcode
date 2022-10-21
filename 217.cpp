a02e0d58a3ed3203a19373727e8bdc00a536667c
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-21 03:01:29 +0300

    Add set solution

--- 217.cpp
+++ 217.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        
         unordered_map<int, int> umap;
+        
         for (int i = 0; i < nums.size(); i++)
             if (++umap[nums[i]] == 2) return true;
+        
         return false;
     }
 };
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.find(nums[i]) != s.end())
+                return true;
+            s.insert(nums[i]);   
+        }
+        return false;
+    }
+};
+
 
