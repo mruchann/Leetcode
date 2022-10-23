96f760d646492c09c436e3ebeeeaabd285c5079e
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-24 00:11:03 +0300

    Update 2447.cpp

--- 2447.cpp
+++ 2447.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int subarrayGCD(vector<int>& nums, int k) {
         
-        int result = 0;
+        int result = 0; // We may not have any subarray.
         
         int n = nums.size();
         
         for (int l = 0; l < n; l++)
         {
-            int current_gcd = nums[l];
+            int current_gcd = nums[l]; // Choose a left anchor, mark it as gcd
             for (int r = l; r < n; r++)
             {
-                current_gcd = gcd(current_gcd, nums[r]);
+                /* 
+                We update our current_gcd value in the inner loop. 
+                If we somehow reach to k at some point, we'll draw an arrow from our left anchor to the that point. 
+                If we found a subarray that has elements with gcd k, increment result.
+                We repeat that steps for each left anchor.
+                */ 
+                current_gcd = gcd(current_gcd, nums[r]); 
                 if (current_gcd == k)
                     result++;
             }
