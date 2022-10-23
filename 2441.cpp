51f4089c67b94024400a13ab3587558cdbcb1db4
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-23 05:02:51 +0300

    Update 2441.cpp

--- 2441.cpp
+++ 2441.cpp
@@ -18,3 +18,18 @@ public:
     }
 };
 
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        
+        unordered_set<int> s(nums.begin(), nums.end()); // load the set
+        
+        int result = -1;
+        
+        for (const int item: s)
+                if (item > 0 && s.find(-item) != s.end()) // check if complement exists
+                    result = max(result, item);
+        
+        return result;
+    }
+};
