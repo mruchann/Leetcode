1b4b17c05e663bfecb91e8603248a7c3b64370e3
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-29 02:20:04 +0300

    Update 69. Sqrt(x).cpp

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -4,22 +4,25 @@ public:
         
         int left = 0;
         int right = x;
+        int middle;
         
-// x is ok since our result will be less than x
-        
-// multiplication operator property:
-        
-// overflow happens even if max x is 2^31 - 1 because middle * middle returns integer
-       
-// middle should be long long
-        
-        long long middle;
+/*
+    I could have used long long type for middle, but I want to use int. Here are the nice tricks:
     
+    int right = x; is ok since our result will be less than x
+        
+    multiplication operator property:
+        
+    overflow happens even if max x is 2^31 - 1 because middle * middle returns integer
+*/
+        
+// #Binary Search Approach
+        
         while (left <= right)
         {
-            middle = (left+right)/2;
+            middle = left + (right-left)/2; // to avoid overflow, we don't add numbers. Now, there is no risk for the overflow.
             
-            if (x == middle*middle)
+            if (x/middle == middle) // if (x == middle * middle) causes overflow for integer type. Look line 16!
                 return middle;
                 
             else if (x < middle*middle)
