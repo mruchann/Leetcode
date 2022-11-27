5a95e905815c1727fafc7eebf75320e13efe7a37
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-11-27 15:03:06 +0300

    Create 98. Validate Binary Search Tree.cpp

---
+++ 98. Validate Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isSubtreeLesser(TreeNode* root, int value)
+    {
+        if (root == nullptr) return true;
+        
+        if (root->val >= value)
+            return false;
+        
+        return isSubtreeLesser(root->left, value) 
+            && isSubtreeLesser(root->right, value);
+    }
+    
+    bool isSubtreeGreater(TreeNode* root, int value)
+    {
+        if (root == nullptr) return true;
+        
+        if (root->val <= value)
+            return false;
+        
+        return isSubtreeGreater(root->left, value) 
+            && isSubtreeGreater(root->right, value);
+    }
+    
+    bool isValidBST(TreeNode* root) 
+    {
+        if (root == nullptr) return true;
+        
+        return isSubtreeLesser(root->left, root->val) // root value is greater than all the nodes in the left
+            && isSubtreeGreater(root->right, root->val) // root value is smaller than all the nodes in the right
+            && isValidBST(root->left) // all subtrees are also BST
+            && isValidBST(root->right); // all subtrees are also BST
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
