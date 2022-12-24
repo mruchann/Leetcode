4f3fd0dc4e4b5810e6e8ebdae4c6edb044a3bc06
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-12-24 20:46:38 +0300

    Update 2511. Maximum Enemy Forts That Can Be Captured.cpp

--- 2511. Maximum Enemy Forts That Can Be Captured.cpp
+++ 2511. Maximum Enemy Forts That Can Be Captured.cpp
@@ -10,9 +10,7 @@ public:
             if (forts[i] == 1)
             {
                 if (minus_one)
-                {
                     res = max(res, captured);
-                }
                 one = true;
                 minus_one = false;
                 captured = 0;
@@ -20,18 +18,14 @@ public:
             else if (forts[i] == -1)
             {
                 if (one)
-                {
                     res = max(res, captured);
-                }
                 minus_one = true;
                 one = false;
                 captured = 0;
             }
-            else // forts[i] == 0
-            {
+            else if (forts[i] == 0)
                 if (one || minus_one)
                     captured++;
-            }
         }
         return res;
     }
