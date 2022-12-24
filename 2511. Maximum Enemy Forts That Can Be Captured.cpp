3c275dd7c30fae7928c8da05ae74493b2097f424
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-12-24 20:10:58 +0300

    Create 2511. Maximum Enemy Forts That Can Be Captured.cpp

---
+++ 2511. Maximum Enemy Forts That Can Be Captured.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int captureForts(vector<int>& forts) 
+    {
+        int res = 0;
+        int captured = 0;
+        bool one = false, minus_one = false; // flags
+        for (int i = 0, n = forts.size(); i < n; i++)
+        {
+            if (forts[i] == 1)
+            {
+                if (minus_one)
+                {
+                    res = max(res, captured);
+                }
+                one = true;
+                minus_one = false;
+                captured = 0;
+            }
+            else if (forts[i] == -1)
+            {
+                if (one)
+                {
+                    res = max(res, captured);
+                }
+                minus_one = true;
+                one = false;
+                captured = 0;
+            }
+            else // forts[i] == 0
+            {
+                if (one || minus_one)
+                    captured++;
+            }
+        }
+        return res;
+    }
+};
