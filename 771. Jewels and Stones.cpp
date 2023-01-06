879f86d79748ed4bc818691027b074d91f354e09
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2023-01-06 15:08:21 +0300

    Create 771. Jewels and Stones.cpp

---
+++ 771. Jewels and Stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) 
+    {
+        unordered_map<char, int> m;
+        int count = 0;
+
+        for (int i = 0; i < jewels.size(); i++)
+            m[jewels[i]]++;
+
+        for (int i = 0; i < stones.size(); i++)    
+            if (m.count(stones[i]))
+                count++;
+        return count;
+    }
+};
